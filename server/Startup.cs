04fee97d3cae453e494031dae7c304259701970b
k0o <sb.knot@gmail.com>
2020-08-27 03:11:46 -0400

    test DB server connection on server init

--- server/Startup.cs
+++ server/Startup.cs
@@ -1,16 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Clusters;
 using server.Models;
 using server.Services;
 
@@ -28,9 +25,13 @@ namespace server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<SRDBSettings>(Configuration.GetSection(nameof(SRDBSettings)));
+            var dbSettings = Configuration.GetSection(nameof(SRDBSettings));
+            services.Configure<SRDBSettings>(dbSettings);
             services.AddSingleton<ISRDBSettings>(sp => sp.GetRequiredService<IOptions<SRDBSettings>>().Value);
             services.AddSingleton<UserService>();
+
+            testDBServerConnection();
+
             services.AddControllers();
         }
 
@@ -50,6 +51,29 @@ namespace server
             {
                 endpoints.MapControllers();
             });
+        }
+
+        public async void testDBServerConnection()
+        {
+            var startTime = DateTime.UtcNow;
+            var client = new MongoClient($"mongodb://localhost/?connectTimeoutMS=5000");
+            try
+            {
+                using (var timeoutCancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMilliseconds(1800)))
+                {
+                    await client.ListDatabasesAsync(timeoutCancellationTokenSource.Token);
+                    Console.ForegroundColor = ConsoleColor.Green; 
+                    Console.WriteLine("[OK] DB connection.");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("[FAILED] DB connection.");
+            }
+
+            Console.ResetColor();
+
         }
     }
 }
